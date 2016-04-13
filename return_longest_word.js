2b94a3503f0eff3cdb5510ba0c2b50e7b718e2fd
Kristen Foster-Marks <kristenfoster-marks@Kristens-MacBook-Pro.local>
2016-04-13 10:46:05 -0700

    Add return longest word algorithm

---
+++ return_longest_word.js
@@ -0,0 +1,16 @@
+/* Return the length of the longest word in the provided sentence. */
+
+function findLongestWord(str) {
+  var strArray = str.split(" ");
+  //console.log(strArray);
+  var longestWordLength = 0;
+  for (i=0; i<strArray.length; i++) {
+     if (strArray[i].length > longestWordLength) {
+         longestWordLength = strArray[i].length;
+         console.log(longestWordLength);
+     }
+  }
+  return longestWordLength;
+}
+
+findLongestWord("Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.");
