e12ec7a391822d793c923df08bae2ed835c6c7de
K-Foster-Marks <kristenlfoster@gmail.com>
2016-04-14 12:47:55 -0700

    Add repl.it link

--- match_keyvalues_object.js
+++ match_keyvalues_object.js
@@ -46,7 +46,7 @@ function whereAreYou(collection, source) {
 	}
   return arr;
 }
-
+//https://repl.it/CFhL/13
 whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 }); /* should return [{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }] */
 //whereAreYou([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }); /*should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }] */
 //whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }); /* should return [{ first: "Tybalt", last: "Capulet" }] */
