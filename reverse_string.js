41e2c54f3a69e1fa551e7e429c66be3afc74ec16
K-Foster-Marks <kristenlfoster@gmail.com>
2016-05-03 18:44:03 -0700

    added non-array method

--- reverse_string.js
+++ reverse_string.js
@@ -1,5 +1,19 @@
 //Reverse the provided string. The result must be a string.
 
+//simpler way to reverse string
+function reverseString(string) {
+	var reversed = "";
+	for (var i = string.length-1; i >= 0; i--) {
+		//console.log(i);
+		//console.log(string[i]);
+		reversed = reversed + string[i];
+		console.log(reversed);
+}
+return reversed;
+}
+reverseString("My cars are AWESOME!");
+
+
 function reverseString(str) {
   var inputString = str;
   var holdingArray = [];
